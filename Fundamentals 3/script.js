a66ce01bc200094b0ea77bac8139648f1c06d0a9
Zak Keith <jmshockey2@gmail.com>
2023-03-18 23:01:38 -0500

    part 3 files

---
+++ Fundamentals 3/script.js
@@ -0,0 +1,15 @@
+function add7(num){
+    return num + 7
+}
+
+function multiply(number1, number2){
+    return number1 * number2
+}
+
+function capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+function lastLetter(string){
+    return string.slice(-1)
+}
